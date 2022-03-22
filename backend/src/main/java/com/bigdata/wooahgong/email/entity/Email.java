4c7859d3b639c10ebd2ee9df416b9d04de27d237
qweadzs <qweadzs@naver.com>
2022-03-22 16:48:51 +0900

    test

---
+++ backend/src/main/java/com/bigdata/wooahgong/email/entity/Email.java
@@ -0,0 +1,34 @@
+package com.bigdata.wooahgong.email.entity;
+
+import com.bigdata.wooahgong.common.util.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Email  extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long authSeq;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String authCode;
+
+    @Column(nullable = false)
+    private LocalDateTime timeLimit;
+
+    @Builder
+    public Email(String email, String authCode, LocalDateTime timeLimit){
+        this.email = email;
+        this.authCode = authCode;
+        this.timeLimit = timeLimit;
+    }
+}
